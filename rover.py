96c976b12fcef51fa66f0e988955057fda30fedb
Edward Jarosiewitz Knapp <edward.jarosiewitzknap1@marist.edu>
2018-01-29 17:45:52 -0500

    Mars Curosity Program

---
+++ rover.py
@@ -0,0 +1,12 @@
+# Mars Curosity Program
+# Distance between Mars and NASA 34 Million Miles
+# Speed of light 186,000
+
+def main():
+    print("Time it takes for photo to reach NASA from Mars Rover")
+    distance = 34000000
+    speed = 186000
+    print("time of travel is", distance/speed, "in seconds")
+    
+main()
+    
