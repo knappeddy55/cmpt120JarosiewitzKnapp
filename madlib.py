023695c027d2f6f3467746d1afae5df6d232a83e
Edward Jarosiewitz Knapp <edward.jarosiewitzknap1@marist.edu>
2018-01-30 21:54:00 -0500

    Added madblib program for lab 2

---
+++ madlib.py
@@ -0,0 +1,15 @@
+def main():
+   print("This is a fun Mablibs activity")
+   noun = input("Enter a noun:")
+   verb = input("Enter a verb:")
+   adjective = input("Enter an adjective:")
+   place = input("Enter a place:")
+
+   print("Take the", noun , "and", verb , "it",  adjective , "at the", place)
+
+main()
+
+
+   
+         
+           
